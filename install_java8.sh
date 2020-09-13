8b0693b4eeb66afdbac95d42efedc7a92c264058
Daniel Gazineu <danielgazineu@gmail.com>
2020-09-13 00:57:41 -0400

    backup for later

--- install_java8.sh
+++ install_java8.sh
@@ -37,3 +37,26 @@ export PATH="\$JAVA_HOME/bin:\$PATH"
 EOF
 
 chmod 755 $JAVA_ENV
+
+
+#if type -p java; then
+#    echo found java executable in PATH
+#    _java=java
+#elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+#    echo found java executable in JAVA_HOME
+#    _java="$JAVA_HOME/bin/java"
+#else
+#    echo "no java"
+#fi
+#
+#if [[ "$_java" ]]; then
+#    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+#    echo version "$version"
+#    major="${version%%.*}"
+#    if [[ "$major" -ge 11 ]]; then
+#        echo version is more than 11
+#    else
+#        echo version is less than 11
+#    fi
+#fi
+
