a8d7e9f0b0c3e69ab2a5edd2e950718989ed2bde
Daniel Gazineu <danielgazineu@gmail.com>
2022-05-31 20:31:31 +0000

    fixed maven url

--- install_maven.sh
+++ install_maven.sh
@@ -6,10 +6,10 @@ source functions.sh
 
 install_command curl
 
-CDN=https://dlcdn.apache.org/maven/maven-3/
+CDN=https://dlcdn.apache.org/maven/maven-3
 
 if [ "$#" -eq 0 ]; then
-  VERSION=$(curl -s $CDN | grep -o "3.[0-9].[0-9]" | sort -ru | head -n 1)
+  VERSION=$(curl -s "$CDN/" | grep -o "3.[0-9].[0-9]" | sort -ru | head -n 1)
   echo "No Maven version was provided, so retrieved latest from download url: $VERSION"
 else
   VERSION=$1
