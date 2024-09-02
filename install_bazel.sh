806b3f279bee6147e3cae3c684b6ca33128dae7c
Daniel Gazineu <danielgazineu@gmail.com>
2024-09-02 19:29:51 -0400

    updated docker install

--- install_bazel.sh
+++ install_bazel.sh
@@ -8,9 +8,8 @@ then
   exit 1
 fi
 
-apt install apt-transport-https curl gnupg
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
-mv bazel.gpg /etc/apt/trusted.gpg.d/
+apt install apt-transport-https curl gnupg -y
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/bazel.gpg
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 
 apt update 
